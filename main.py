ed8888970bb0d6b3ec1de88dbd5885497142f1ae
huLasPet <kenwoodo@hotmail.com>
2022-01-29 10:43:47 +0100

    Upgrade to Postgres

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import gravatarurl as avatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
